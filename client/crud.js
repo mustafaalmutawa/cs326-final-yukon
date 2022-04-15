b7ceb1ed9e3f3d1034ecf8c9c56eb20b22fc7fa7
nbodicharla <nbodicharla@umass.edu>
2022-04-15 15:48:17 -0400

    working post request for login

--- client/crud.js
+++ client/crud.js
@@ -1,9 +1,10 @@
 export async function login(username, password) {
   const user = {'username': username, 'password': password};
-  const response = await fetch(
-    '/user/login',
-    {
+  const response = await fetch(`/user/login`,{
       method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
       body: JSON.stringify(user)
     }
   );
