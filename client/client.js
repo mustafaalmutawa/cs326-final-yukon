33207abdb2d8d10221346bf35ec21498f60dfdee
alexrohrberg <alexrohrberg@gmail.com>
2022-04-15 17:04:45 -0400

    touched up fixes

--- client/client.js
+++ client/client.js
@@ -3,19 +3,10 @@ import * as crud from './crud.js';
 const login_button = document.getElementById('login_button');
 const email_field = document.getElementById('login_email');
 const password_field = document.getElementById('login_password');
-email_field.addEventListener('input', function(){
-  email_field.value = document.getElementById("login_email").value
-  console.log(email_field.value);
-});
-password_field.addEventListener('input', function(){
-  password_field.value = document.getElementById("login_password").value
-  console.log(password_field.value);
-});
+
 login_button.addEventListener('click', async (e) => {
   const email = email_field.value;
   const password = password_field.value;
-  console.log(email)
-  console.log(password)
   await crud.login(email, password);
 });
 
