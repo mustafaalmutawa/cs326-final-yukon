3910b9f1f71402aab9e869ac421e20741d813b0b
Tatyana Edozie <tatyanaedozie@gmail.com>
2022-04-18 21:18:31 -0400

    fixed persistence and image display issues

--- client/Homepage.js
+++ client/Homepage.js
@@ -1,22 +1,53 @@
-$(document).ready(function() {
+
+$(document).ready(async function() {
+    //localStorage.removeItem("homepage_listings")
+    reload();
     const product = JSON.parse(localStorage.getItem("product"));
-    const image_files = JSON.parse(localStorage.getItem("image_files"));
-    if (product !== null && image_files !== null) {
+    const urls = JSON.parse(localStorage.getItem("image_files"));
+    const final_images = [];
+
+    if (product !== null && urls !== null) {
+        for (const url of urls) {
+            const image = new Image();
+            image.src = url;
+            final_images.push(image);
+        }
         localStorage.removeItem("product");
         localStorage.removeItem("image_files");
-        addHomepageListing(product, image_files);
+        addHomepageListing(product, final_images);
     }
 });
 
-export function goToHomepage(product, image_files) {
+function imagesToURLsHelper(image) {
+    return new Promise(function (resolve, reject) {
+        let fr = new FileReader();
+        fr.onload = function (event) {
+            resolve(event.target.result);
+        }
+        fr.readAsDataURL(image);
+    });
+}
+
+async function imagesToURLs(image_files) {
+    let images_arr = Array.prototype.slice.call(image_files);
+    try {
+        const arr = await Promise.all(images_arr.map(image => imagesToURLsHelper(image)));
+        return arr;
+    } catch (err) {
+        console.log(err);
+    }  
+}
+
+export async function goToHomepage(product, image_files) {
+    const urls_arr = await imagesToURLs(image_files);
     localStorage.setItem("product", JSON.stringify(product));
-    localStorage.setItem("image_files", JSON.stringify(image_files));
+    localStorage.setItem("image_files", JSON.stringify(urls_arr));
     document.location = "/homepage";
 }
 
 let cur_num = 1;
 
-function addHomepageListing(product, image_files) {
+function addHomepageListing(product, image_nodes) {
     const grid_row_wrapper = document.getElementById("real_row");
     const listing_wrapper = document.createElement("div");
     listing_wrapper.setAttribute("class", "col-md-3 img-thumbnail");
@@ -32,8 +63,8 @@ function addHomepageListing(product, image_files) {
     inner_carousel.setAttribute("class", "carousel-inner");
     carousel.appendChild(inner_carousel);
 
-    for (let i = 0; i < image_files.length; i++) {
-        const img = image_files[i];
+    for (let i = 0; i < image_nodes.length; i++) {
+        const img_node = image_nodes[i];
         const cur_img = document.createElement("div");
 
         if (i === 0)
@@ -41,12 +72,7 @@ function addHomepageListing(product, image_files) {
         else
             cur_img.setAttribute("class", "carousel-item");
 
-        const img_node = document.createElement("img");
-        img_node.setAttribute("src", window.URL.createObjectURL(img));
         img_node.setAttribute("class", "d-block w-100");
-        img_node.onload = function() {
-            URL.revokeObjectURL(this.src);
-        }
         cur_img.appendChild(img_node);
         inner_carousel.appendChild(cur_img);
     }
@@ -68,10 +94,15 @@ function addHomepageListing(product, image_files) {
     h1.appendChild(t1);
     h2.appendChild(t2);
 
+    h1.setAttribute("style", "font-size: 21px;");
+    h2.setAttribute("style", "font-size: 17px;");
+
     listing_wrapper.appendChild(h1);
     listing_wrapper.appendChild(h2);
 
     grid_row_wrapper.appendChild(listing_wrapper);
+    let listings = grid_row_wrapper.innerHTML;
+    localStorage.setItem("homepage_listings", listings);
 }
 
 function makeButton(c1, c2, ds, text, cur_num) {
@@ -94,4 +125,13 @@ function makeButton(c1, c2, ds, text, cur_num) {
     button.appendChild(text_stuff);
 
     return button;
+}
+
+function reload() {
+    const grid_row_wrapper = document.getElementById("real_row");
+    const contents = localStorage.getItem("homepage_listings");
+    if (contents !== null)
+        grid_row_wrapper.innerHTML = contents;
+    else
+        grid_row_wrapper.innerHTML = "";
 }
