8df3b9d6fc464c1229135855903d63373851d262
nbodicharla <nbodicharla@umass.edu>
2022-04-28 15:14:01 -0400

    setting up server

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ import users from './users.js';
 import auth from './auth.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { Database } from './database.js';
 
 // We will use __dirname later on to send files back to the client.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(dirname(__filename));
 
-// Create the Express app
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Session configuration
 const sessionConfig = {
   // set this encryption key in Heroku config (never in GitHub)!
   secret: process.env.SECRET || 'SECRET',
@@ -22,19 +18,21 @@ const sessionConfig = {
   saveUninitialized: false,
 };
 
-// Setup the session middleware
-app.use(expressSession(sessionConfig));
-// Allow JSON inputs
-app.use(express.json());
-// Allow URLencoded data
-app.use(express.urlencoded({ extended: true }));
-// Allow static file serving
-app.use(express.static('client'));
-// Configure our authentication strategy
-auth.configure(app);
+const app = express();
+const port = process.env.PORT || 3000;
 
-// Our own middleware to check if the user is authenticated
-function checkLoggedIn(req, res, next) {
+class Server {
+  constructor(dburl) {
+    this.dburl = dburl;
+    this.app = express();
+    this.app.use('/', express.static('client'));
+    this.pp.use(expressSession(sessionConfig));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.static('client'));
+    auth.configure(app);
+  }
+ checkLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     // If we are authenticated, run the next route.
     next();
@@ -43,81 +41,55 @@ function checkLoggedIn(req, res, next) {
     res.redirect('/login');
   }
 }
+  async initRoutes() {
+    const self = this;
+    this.app.get('/product', async (request, response) => {
+      response.sendFile('/client/product.html', {root: __dirname })
+    });
+    this.app.post('/product/new', async (req, res) => {
+    });
+    this.app.post('/product/buy', async (req, res) => {
+    });
+    this.app.get('/user', async (req, res) => {
+      response.sendFile('/client/user_profile.html', {root: __dirname })
 
-app.get('/', checkLoggedIn, (req, res) => {
-  res.send('hello world');
-});
-
-// Handle the URL /login (just output the login.html file).
-app.get('/login', (req, res) =>
-  res.sendFile('client/login.html', { root: __dirname })
-);
-
-// Handle post data from the login.html form.
-app.post(
-  '/login',
-  auth.authenticate('local', {
-    // use username/password authentication
-    successRedirect: '/private', // when we login, go to /private
-    failureRedirect: '/login', // otherwise, back to login
-  })
-);
-
-// Handle logging out (takes us back to the login page).
-app.get('/logout', (req, res) => {
-  req.logout(); // Logs us out!
-  res.redirect('/login'); // back to login
-});
-
-// Like login, but add a new user and password IFF one doesn't exist already.
-// If we successfully add a new user, go to /login, else, back to /register.
-// Use req.body to access data (as in, req.body['username']).
-// Use res.redirect to change URLs.
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-  if (users.addUser(username, password)) {
-    res.redirect('/login');
-  } else {
-    res.redirect('/register');
+    });
+    this.app.post('/user/new', async (req, res) => {
+    });
+    this.app.put('/user/update', async (req, res) => {
+    });
+    this.app.delete('/user/delete', async (request, response) => {
+    });
+    this.app.get('/login', async (request, response) => {
+      response.sendFile('/client/Login.html', {root: __dirname })
+    });
+    this.app.get('/register', async (request, response) => {
+      response.sendFile('/client/register.html', {root: __dirname })
+    });
+    this.app.get(['/homepage', '/'], async (request, response) => {
+      response.sendFile('/client/Homepage.html', {root: __dirname })
+    });
+    this.app.get('/listing', async (request, response) => {
+      response.sendFile('/client/listing.html', {root: __dirname })
+    });
+  
+   
   }
-});
 
-// Register URL
-app.get('/register', (req, res) =>
-  res.sendFile('client/register.html', { root: __dirname })
-);
-
-// Private data
-app.get(
-  '/private',
-  checkLoggedIn, // If we are logged in (notice the comma!)...
-  (req, res) => {
-    // Go to the user's page.
-    res.redirect('/private/' + req.user);
+  async initDb() {
+    this.db = new Database(this.dburl);
+    await this.db.connect();
   }
-);
 
-// A dummy page for the user.
-app.get(
-  '/private/:userID/',
-  checkLoggedIn, // We also protect this route: authenticated...
-  (req, res) => {
-    // Verify this is the right user.
-    if (req.params.userID === req.user) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write('<H1>HELLO ' + req.params.userID + '</H1>');
-      res.write('<br/><a href="/logout">click here to logout</a>');
-      res.end();
-    } else {
-      res.redirect('/private/');
-    }
+  async start() {
+    await this.initRoutes();
+    await this.initDb();
+    const port = process.env.PORT || 3000;
+    this.app.listen(port, () => {
+      console.log(`PeopleServer listening on port ${port}!`);
+    });
   }
-);
+}
 
-app.get('*', (req, res) => {
-  res.send('Error');
-});
-
-app.listen(port, () => {
-  console.log(`App now listening at http://localhost:${port}`);
-});
+const server = new Server(process.env.DATABASE_URL);
+server.start();
