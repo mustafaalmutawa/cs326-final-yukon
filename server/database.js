99846539f8ebe8d0e7b3b5adc1a7fee1137718d2
Tatyana Edozie <tatyanaedozie@gmail.com>
2022-04-30 20:36:04 -0400

    removed example code, added function headers, added body for some functions

--- server/database.js
+++ server/database.js
@@ -2,80 +2,83 @@ import 'dotenv/config';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 export class Database {
-  constructor(dburl) {
-    this.dburl = dburl;
-  }
-
-  async connect() {
-    this.client = await MongoClient.connect(this.dburl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverApi: ServerApiVersion.v1,
-    });
-
-    // Get the database.
-    this.db = this.client.db('user');
-
-    // Init the database.
-    await this.init();
-  }
-
-  async init() {
-    this.collection = this.db.collection('user');
-
-    const count = await this.collection.countDocuments();
-
-    if (count === 0) {
-      await this.collection.insertMany([
-        { _id: '1', name: 'Artemis', age: 19 },
-        { _id: '2', name: 'Parzival', age: 17 },
-        { _id: '3', name: 'John', age: 30 },
-        { _id: '4', name: 'Mia', age: 22 },
-      ]);
+    constructor(dburl) {
+        this.dburl = dburl;
     }
-  }
 
-  // Close the pool.
-  async close() {
-    this.client.close();
-  }
+    async connect() {
+        this.client = await MongoClient.connect(this.dburl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1,
+        });
 
-  // CREATE a user in the database.
-  async createPerson(id, name, age) {
-    const res = await this.collection.insertOne({ _id: id, name, age });
-    // Note: the result received back from MongoDB does not contain the
-    // entire document that was inserted into the database. Instead, it
-    // only contains the _id of the document (and an acknowledged field).
-    return res;
-  }
+        // Get the database.
+        this.db = this.client.db('ProductsUsersDB');
 
-  // READ a user from the database.
-  async readPerson(id) {
-    const res = await this.collection.findOne({ _id: id });
-    return res;
-  }
+        // Init the database.
+        await this.init();
+    }
 
-  // UPDATE a user in the database.
-  async updatePerson(id, name, age) {
-    const res = await this.collection.updateOne(
-      { _id: id },
-      { $set: { name, age } }
-    );
-    return res;
-  }
+    async init() {
+        this.products = this.db.collection('products');
+        this.users = this.db.collection('users');
+    }
 
-  // DELETE a user from the database.
-  async deletePerson(id) {
-    // Note: the result received back from MongoDB does not contain the
-    // entire document that was deleted from the database. Instead, it
-    // only contains the 'deletedCount' (and an acknowledged field).
-    const res = await this.collection.deleteOne({ _id: id });
-    return res;
-  }
+    // Close the pool.
+    async close() {
+        this.client.close();
+    }
 
-  // READ all people from the database.
-  async readAllPeople() {
-    const res = await this.collection.find({}).toArray();
-    return res;
-  }
+    async createProduct(itemName, price, category, condition, description, 
+    images, location, shipping, shippingPrice, pickup, payment) {
+
+        const res = await this.products.insertOne({itemName, price, 
+        category, condition, description, images, location, shipping, 
+        shippingPrice, pickup, payment});
+
+        // Note: the result received back from MongoDB does not contain the
+        // entire document that was inserted into the database. Instead, it
+        // only contains the _id of the document (and an acknowledged field).
+        return res;
+    }
+
+    async createUser(email, password) {
+        const res = await this.users.insertOne({email, password});
+        return res;
+    }
+
+    async getProduct(id) {
+
+    }
+
+    async getUser(id) {
+
+    }
+
+    async updateProduct(id) {
+
+    }
+
+    async updateUser(id) {
+
+    }
+
+    async deleteProduct(id) {
+
+    }
+
+    async deleteUser(id) {
+
+    }
+
+    async getAllProducts() {
+        const res = await this.products.find({}).toArray();
+        return res;
+    }
+
+    async getAllUsers() {
+        const res = await this.users.find({}).toArray();
+        return res;
+    }
 }
