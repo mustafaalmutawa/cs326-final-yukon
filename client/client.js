e17c53de92729ef52e5c609f86dca8e0f3a66e9c
mustafaalmutawa <mustafa.almutawa@outlook.com>
2022-05-02 18:29:44 +0300

    added update_product buttons, still need to complete functionality

--- client/client.js
+++ client/client.js
@@ -7,6 +7,10 @@ const password_field = document.getElementById('login_password');
 const listing_button = document.getElementById("create_listing");
 const delete_button = document.getElementById("delete_button")
 const updateName_button = document.getElementById('update_name');
+const updateListing_button = document.getElementById('update_listing');
+const userProducts_num = document.getElementsByClassName('product').length;
+let productToUpdate = 0;
+
 
 
 if (login_button !== null) {
@@ -14,7 +18,7 @@ if (login_button !== null) {
     const email = email_field.value;
     const password = password_field.value;
     await crud.login(email, password);
-    window.location.href = "Homepage.html"
+    window.location.href = "Homepage.html";
   });
 }
 
@@ -24,14 +28,14 @@ if (register_button !== null) {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     await crud.register(username,email, password);
-    window.location.href = "user_profile.html"
+    window.location.href = "user_profile.html";
   });
 }
 
 if (delete_button !== null) {
-  delete_button.addEventListener('click', async(e) =>{
+  delete_button.addEventListener('click', async(e) => {
   await crud.deleteUser();
-  window.location.href = "register.html"
+  window.location.href = "register.html";
 })
 }
 
@@ -63,7 +67,7 @@ if (listing_button !== null) {
     const payment = Array.from(selectedPayment).map(o => o.value);
 
     const product_data = await crud.createProduct(itemName, price, category, condition, description, images, location, shipping, shippingPrice, pickup, payment);
-    window.location.href = "user_profile.html"
+    window.location.href = "user_profile.html";
   });
 }
 
@@ -72,5 +76,47 @@ if (updateName_button !== null) {
     const name = document.getElementById('newName').value;
     document.getElementById('user_name').innerHTML = name;
     await crud.updateName(name);
+  });
+}
+
+for (let i = 1; i <= userProducts_num; i++) {
+  const updateProduct_button = document.getElementById(`updateProduct${i}`);
+  if (updateProduct_button !== null) {
+    updateProduct_button.addEventListener('click', async (e) => {
+      productToUpdate = i;
+      window.location.href = "update_listing.html";
+    console.log(productToUpdate);
+    });
+  }
+}
+
+if (updateListing_button !== null) {
+  updateListing_button.addEventListener('click', async (e) => {
+    const itemName = document.getElementById("itemName").value;
+    const price = document.getElementById("price").value;
+    console.log("name: " + itemName);
+    console.log("price: " + price);
+
+    const selectedCategory = document.getElementById("category");
+    const category = selectedCategory.options[selectedCategory.selectedIndex].text;
+
+    const selectedCondition = document.getElementById("condition");
+    const condition = selectedCondition.options[selectedCondition.selectedIndex].text;
+
+    const description = document.getElementById("description").value;
+    const images = document.getElementById("images").files;
+
+    const selectedLocation = document.getElementById("location");
+    const location = selectedLocation.options[selectedLocation.selectedIndex].text;
+
+    const shipping = document.getElementById("shipping").checked;
+    let shippingPrice = null;
+    if (shipping) {
+      shippingPrice = document.getElementById("shipping_price").value;
+    }
+
+    const pickup = document.getElementById("pickup").checked;
+    const selectedPayment = document.getElementById("pPayement").selectedOptions
+    const payment = Array.from(selectedPayment).map(o => o.value);
   });
 }
