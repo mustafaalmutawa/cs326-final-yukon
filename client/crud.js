d2a55aed449b2d8f10540fb506151123a8ccaf38
Tatyana Edozie <tatyanaedozie@gmail.com>
2022-05-05 12:21:00 -0400

    fixed returned data to product id

--- client/crud.js
+++ client/crud.js
@@ -42,7 +42,7 @@ export async function createProduct(itemName,price,category,condition,descriptio
     }
   );
   const data = await response.json();
-  return data;
+  return data.id;
 }
 
 export async function getUserProfile() {
